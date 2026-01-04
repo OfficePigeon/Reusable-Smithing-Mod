344468879add38d8588524cf6683f508d3c397ab
OfficePigeon <ava@wich.fun>
2026-01-04 00:03:59 -0500

    Cross-version support back to 1.20

--- src/main/java/fun/wich/mixin/ReusableSmithingSmithingScreenHandlerMixin.java
+++ src/main/java/fun/wich/mixin/ReusableSmithingSmithingScreenHandlerMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.screen.ForgingScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.SmithingScreenHandler;
-import net.minecraft.screen.slot.ForgingSlotsManager;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SmithingScreenHandler.class)
 public abstract class ReusableSmithingSmithingScreenHandlerMixin extends ForgingScreenHandler {
-	public ReusableSmithingSmithingScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context, ForgingSlotsManager forgingSlotsManager) {
-		super(type, syncId, playerInventory, context, forgingSlotsManager);
+	public ReusableSmithingSmithingScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
+		super(type, syncId, playerInventory, context);
 	}
 	@Inject(method="decrementStack", at=@At("HEAD"), cancellable=true)
 	private void ReusableSmithing_decrementStack(int slot, CallbackInfo ci) {
